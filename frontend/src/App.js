356acb6f307f744f4059417c70abf48466f6f6cc
samuellye98 <sl2982@cornell.edu>
2019-11-24 00:59:49 -0500

    responsive navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,17 @@
 import React, { Component } from 'react';
-import Navbar from './components/layout/Navbar';
+import NavigationBar from './components/layout/Navbar';
 import SignUp from './components/signup/SignUp';
 import Home from './components/Home';
 import FAQ from './components/Faq';
 import { Route } from 'react-router-dom';
 import './app.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        <Navbar />
+        <NavigationBar />
         <Route exact={true} path={'/'} component={Home} />
         <Route exact={true} path={'/home'} component={Home} />
         <Route exact={true} path={'/faq'} component={FAQ} />
